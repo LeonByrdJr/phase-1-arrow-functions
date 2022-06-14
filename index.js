069b6bb88ad72b891d129414dd50f1b8f3c85d74
leonbyrdjr <leonbyrdjr@Leons-MacBook-Pro.local>
2022-06-13 23:19:23 -0500

    commit tree 70517f64cd6984d7d3574179925560b8b66c0bce

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (number, number2) => {
+    return 2000 / 100
+}
+
+const square = number => {
+    return number ** 2
+}
+
+const add = (number, number2) => {
+    return number + number2
+}
